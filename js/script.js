6a254b4eb65adcee23b655469bf939f48c3fc6c0
smith109 <devon.smith017@gmail.com>
2024-10-20 20:06:51 -0500

    Add an object to control the flow of the game

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ const gameBoard = (function () {
   const clearBoard = () => board.fill('');
   return { addMarker, getBoard, clearBoard };
 })();
+
+const gameController = (function () {
+  const playerOne = createPlayer('Player One', 'X');
+  const playerTwo = createPlayer('Player Two', 'O');
+  let currentPlayer = playerOne;
+
+  const switchPlayerTurn = () => {
+    currentPlayer = currentPlayer === playerOne ?
+      playerTwo : playerOne;
+  }
+
+  const playRound = (cell) => {
+    gameBoard.addMarker(cell, currentPlayer.getMarker());
+    switchPlayerTurn();
+    console.log(gameBoard.getBoard());
+  }
+
+  return { playRound };
+})();
