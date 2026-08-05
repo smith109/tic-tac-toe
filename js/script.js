8f6c28a38703c07201731bf0b52ef5df423c7a6a
smith109 <devon.smith017@gmail.com>
2026-08-05 09:34:09 -0500

    Add a module to manage the game's logic

--- js/script.js
+++ js/script.js
@@ -17,3 +17,29 @@ const gameBoard = (() => {
 
   return { getBoard, clearBoard, isFull, addMarker };
 })();
+
+const gameController = (() => {
+  const players = [
+    createPlayer('Player One', 'X'),
+    createPlayer('Player Two', 'O')
+  ];
+
+  const [playerOne, playerTwo] = players;
+  let activePlayer = playerOne;
+
+  const getActivePlayer = () => activePlayer;
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+  };
+
+  const playRound = (cell) => {
+    const board = gameBoard.getBoard();
+    if (board[cell] !== '') return;
+
+    gameBoard.addMarker(cell, activePlayer.getMarker());
+    switchPlayerTurn();
+  };
+
+  return { getActivePlayer, playRound };
+})();
